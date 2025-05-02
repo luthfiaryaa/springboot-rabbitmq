e02aea6ed9bcc65240fa8c92e2cdbc2dcbb78088
luthfiaryaa <luthfi.aryarizki@mindigitalgroup.com>
2025-05-02 14:54:41 +0700

    init commit

---
+++ src/main/java/com/test/springbootrabbitmq/config/RabbitMQConfig.java
@@ -0,0 +1,35 @@
+package com.test.springbootrabbitmq.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author luthfi aryarizki
+ * @date 2025/05/02 13:00
+ */
+@Configuration
+public class RabbitMQConfig {
+
+    public static final String QUEUE_NAME = "myQueue";
+    public static final String EXCHANGE_NAME = "myExchange";
+
+    @Bean
+    public Queue queue() {
+        return new Queue(QUEUE_NAME, false);
+    }
+
+    @Bean
+    public TopicExchange exchange() {
+        return new TopicExchange(EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("routing.key.#");
+    }
+
+}
